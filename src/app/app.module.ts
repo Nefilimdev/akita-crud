e63c46c4ff7eef2f302d85044f24109cdd030f7b
Caio Alves <caiojuodzevicius@gmail.com>
2021-01-27 02:25:58 -0300

    feat: rotas definidas e associadas aos componentes correspondentes a essas rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { CourseModule } from './course/course.module';
 
 import { AppComponent } from './app.component';
+import { CoursesListComponent } from './course/components/courses-list/courses-list.component';
+import { CreateCourseComponent } from './course/components/create-course/create-course.component';
+
+const routes = [
+  {
+    path: 'courses',
+    component: CoursesListComponent
+  },
+  {path: 'create-course', component: CreateCourseComponent},
+  {path: '**', redirectTo: 'courses'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CourseModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
