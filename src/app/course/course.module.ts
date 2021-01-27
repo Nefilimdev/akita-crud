6a708065cbffa002486237f59b7c89955ab5ed92
Caio Alves <caiojuodzevicius@gmail.com>
2021-01-27 02:21:54 -0300

    feat: componentes declarados.

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseService } from './services/course.service';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CreateCourseComponent } from './components/create-course/create-course.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
+  declarations: [CoursesListComponent, CreateCourseComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule
   ],
-  declarations: [CourseService]
+  providers: [CourseService],
+  exports: [CoursesListComponent, CreateCourseComponent]
 })
 export class CourseModule { }
