314a20b0b839abf803ccd50c4620883931093fb1
Caio Alves <caiojuodzevicius@gmail.com>
2021-01-27 02:10:22 -0300

    feat: lógica do CRUD da lista de cursos criada.

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseService } from './services/course.service';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 @NgModule({
   imports: [
